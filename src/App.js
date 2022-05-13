68dd92defd7062ca33efc5696e5cb56f08a84f7f
harrishalisha <noranalisha@gmail.com>
2022-05-13 07:29:08 +0100

    Changing Header

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HELLO...This is my Fist React Amplify App
         </a>
       </header>
     </div>
